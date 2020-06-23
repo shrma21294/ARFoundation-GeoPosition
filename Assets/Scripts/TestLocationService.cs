b965d34759ab01314cb2ad8de6205f1e26d0c226
shrma21294 <komalsharma21294@gmail.com>
2020-06-22 22:53:27 -0400

    pushing the project

---
+++ Assets/Scripts/TestLocationService.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLocationService : MonoBehaviour
+{
+
+    public void CallingStart()
+    {
+        Debug.Log("Inside CallingStart");
+        Input.location.Start();
+
+    }
+    IEnumerator Start()
+    {
+        Debug.Log("Inside Start");
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("checking enabled by user "+ Input.location.isEnabledByUser);
+            yield break;
+        }
+            
+
+        // Start service before querying location
+        Input.location.Start(1.0f,0.1f);
+
+        Debug.Log("called Start");
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            //print("Timed out");
+            Debug.Log("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+           // print("Unable to determine device location");
+            Debug.Log("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            //print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+            Debug.Log("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }
+}
